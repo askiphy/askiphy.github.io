78e700592519f8573f7e29b3bbc1c355d53847fa
Zamur650 <Zamur_yt@mail.ru>
2021-05-01 11:04:40 +0700

    Rainbow added

---
+++ index.js
@@ -0,0 +1,17 @@
+let h1text = document.getElementById("h1text");
+let h3text = document.getElementById("h3text");
+let image = document.getElementById("image");
+
+let color = 0;
+
+setInterval(function () {
+  h1text.style['text-decoration-color'] = `hsl(${color}, 100%, 50%)`;
+  h3text.style['text-decoration-color'] = `hsl(${color}, 100%, 50%)`;
+  image.style['border-color'] = `hsl(${color}, 100%, 50%)`;
+
+  color += 1;
+
+  if (color == 360) {
+    color = 0;
+  }
+}, 60);
