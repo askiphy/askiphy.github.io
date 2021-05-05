5a159ecdde3a6f38b460b64cbb56b8e8d826c166
Zamur650 <Zamur_yt@mail.ru>
2021-05-05 20:19:29 +0700

    Mobile optimization

--- index.dev.js
+++ index.dev.js
@@ -1,15 +1,16 @@
 'use strict';
 
 var h1text = document.getElementById("h1text");
-var h3text = document.getElementById("h3text");
+var h2text = document.getElementById("h2text");
 var image = document.getElementById("image");
 var position = document.getElementById("position");
+var stepOfGradient = 22.5;
 var color = 0;
 setInterval(function () {
   h1text.style['text-decoration-color'] = "hsl(".concat(color, ", 100%, 50%)");
-  h3text.style['text-decoration-color'] = "hsl(".concat(color, ", 100%, 50%)");
+  h2text.style['text-decoration-color'] = "hsl(".concat(color, ", 100%, 50%)");
   image.style['border-color'] = "hsl(".concat(color, ", 100%, 50%)");
-  position.style['background'] = "linear-gradient(45deg, hsl(".concat(color - 45, ", 100%, 50%) 0%, hsl(").concat(color, ", 100%, 50%) 50%, hsl(").concat(color + 45, ", 100%, 50%) 100%)");
+  position.style['background'] = "linear-gradient(45deg, hsl(".concat(color - stepOfGradient * 2, ", 100%, 50%) 0%, hsl(").concat(color - stepOfGradient, ", 100%, 50%) 25%, hsl(").concat(color, ", 100%, 50%) 50%, hsl(").concat(color + stepOfGradient, ", 100%, 50%) 75%, hsl(").concat(color + stepOfGradient * 2, ", 100%, 50%) 100%)");
   color += 1;
 
   if (color == 360) {
